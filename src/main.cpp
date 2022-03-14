e01aad39323db98428b5e008b7013b41c7317b9c
Kye Morton <pryre.dev@outlook.com>
2022-03-14 22:23:08 +1000

    Trying to get lib working

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,71 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+// #include <libopencm3/stm32/usart.h>
+// #include <libopencm3/stm32/dma.h>
+// #include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/spi.h>
+
+#include <OneBitDisplay.h>
+
 
 #define RCCLEDPORT (RCC_GPIOC)
 #define LEDPORT (GPIOC)
 #define LEDPIN (GPIO13)
+
+OBDISP obd;
+#define FLIP180 0
+#define INVERT 0
+#define BITBANG 1
+#define DC_PIN 22
+#define CS_PIN 3
+#define RESET_PIN 1
+#define MOSI_PIN 19
+#define CLK_PIN 23
+#define LED_PIN 18
+
+
+static void spi_init(void) {
+	/* Enable the GPIO ports whose pins we are using */
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN,
+			GPIO3 | GPIO4 | GPIO5);
+	gpio_set_af(GPIOB, GPIO_AF5, GPIO3 | GPIO4 | GPIO5);
+	gpio_set_output_options(GPIOF, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ,
+				GPIO7 | GPIO9);
+
+	/* Chip select line */
+	// gpio_set(GPIOC, GPIO1);
+	// gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+
+	rcc_periph_clock_enable(RCC_SPI1);
+
+	//Configure SPI
+	uint32_t cr_tmp = SPI_CR1_BAUDRATE_FPCLK_DIV_8 |
+		 SPI_CR1_MSTR |
+		 SPI_CR1_SPE |
+		 SPI_CR1_CPHA |
+		 SPI_CR1_CPOL_CLK_TO_1_WHEN_IDLE;
+
+	//Initialise SPI
+	SPI_CR2(SPI1) |= SPI_CR2_SSOE;
+	SPI_CR1(SPI1) = cr_tmp;
+}
+
+static void write_reg(uint8_t reg, uint8_t value)
+{
+	gpio_clear(GPIOC, GPIO1); /* CS* select */
+	spi_send(SPI5, reg);
+	(void) spi_read(SPI5);
+	spi_send(SPI5, value);
+	(void) spi_read(SPI5);
+	gpio_set(GPIOC, GPIO1); /* CS* deselect */
+	return;
+}
 
 static void gpio_setup(void)
 {
@@ -33,17 +92,23 @@ static void gpio_setup(void)
 	rcc_periph_clock_enable(RCCLEDPORT);
 	/* Set pin to 'output push-pull'. */
 	/* Using API functions: */
-#ifdef STM32F1
-    gpio_set_mode(LEDPORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDPIN);
-#else
 	gpio_mode_setup(LEDPORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDPIN);
-#endif
 }
+
 
 int main(void)
 {
+	// put your setup code here, to run once:
+	//void obdSPIInit(OBDISP *pOBD, int iType, int iDC, int iCS, int iReset, int iMOSI, int iCLK, int iLED, int bFlip, int bInvert, int bBitBang, int32_t iSpeed)
+	obdSPIInit(&obd, LCD_UC1701, DC_PIN, CS_PIN, RESET_PIN, MOSI_PIN, CLK_PIN, LED_PIN, FLIP180, INVERT, BITBANG, 0);
+	obdFill(&obd, 0, 1);
+	// put your main code here, to run repeatedly:
+
 	int i;
 	gpio_setup();
+
+	obdWriteString(&obd, 0, 0, 0, (char *)"Hello World!", FONT_8x8, 0, 1);
+
 	/* Blink the LED on the board. */
 	while (1) {
 		/* Using API function gpio_toggle(): */
